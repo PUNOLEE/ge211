7873320d2dcbcf158584fa75bd94fec289860d8e
Jesse A. Tov <jesse.tov@gmail.com>
2018-08-29 08:51:03 -0500

    Removed audio initialization warning. Why doesn't it work?

--- src/ge211_geometry.h
+++ src/ge211_geometry.h
@@ -6,14 +6,8 @@
 
 namespace ge211 {
 
-/// \defgroup Geometry Geometric objects and operations
-/// @{
-
 /// Geometric objects and their operations.
 namespace geometry {
-
-/// \defgroup Dimensions Dimensions and their operations
-/// @{
 
 /// Represents the dimensions of an object, or more generally,
 /// the displacement between two Basic_position%s. Note that
@@ -178,11 +172,6 @@ Basic_dimensions<T>& operator/=(Basic_dimensions<T>& d1, double s2)
 {
     return d1 = d1 / s2;
 }
-
-/// @}
-
-/// \defgroup Positions Positions and their operations
-/// @{
 
 /// A position in the T-valued Cartesian plane. In graphics,
 /// the origin is traditionally in the upper left, so the *x* coordinate
@@ -355,11 +344,6 @@ Basic_position<T>& operator-=(Basic_position<T>& p1, Basic_dimensions<T> d2)
     return p1 = p1 - d2;
 }
 
-/// @}
-
-/// \defgroup Rectangles Rectangles and their operations
-/// @{
-
 /// Represents a positioned rectangle.
 template <class T>
 struct Basic_rectangle
@@ -499,11 +483,6 @@ bool operator!=(const Basic_rectangle<T> &r1,
     return !(r1 == r2);
 }
 
-/// @}
-
-/// \defgroup Transforms Transforms and their operations
-/// @{
-
 /// A rendering transform, which can scale, flip, and rotate. A Transform
 /// can be given to
 /// Sprite_set::add_sprite(const Sprite&, Position, int, const Transform&)
@@ -633,10 +612,6 @@ private:
 bool operator==(const Transform&, const Transform) noexcept;
 /// Disequality for transforms.
 bool operator!=(const Transform&, const Transform) noexcept;
-
-/// @}
-
-/// @}
 
 } // end namespace geometry.
 
